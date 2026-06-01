a576f6ef05212892ee19f1ab4e9541eec89591c1
Devansh27112001 <dkansara2711@gmail.com>
2026-06-01 12:27:08 -0400

    initial commit

---
+++ 01-starting-project/src/app/user/user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../utility/users';
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  selectedUser = DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)];
+}
