195332d281d09694b965e6c17d5bffc37e5835ee
Devansh27112001 <dkansara2711@gmail.com>
2026-06-01 13:07:01 -0400

    Using signals

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { DUMMY_USERS } from '../utility/users';
 @Component({
   selector: 'app-user',
@@ -8,5 +8,18 @@ import { DUMMY_USERS } from '../utility/users';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)];
+  private getRandomIndex(): number {
+    return Math.floor(Math.random() * DUMMY_USERS.length);
+  }
+
+  selectedUser = signal(DUMMY_USERS[this.getRandomIndex()]);
+
+  get imagePath(): string {
+    return 'assets/users/' + this.selectedUser().avatar;
+  }
+
+  onSelectUser() {
+    this.selectedUser.set(DUMMY_USERS[this.getRandomIndex()]);
+    console.log('click');
+  }
 }
